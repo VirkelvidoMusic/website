2b72ac0772118fddb26a2f0ad4ca562c12c0b444
chlkrisz <64039807+chlkrisz@users.noreply.github.com>
2025-01-29 21:03:34 +0100

    xd

--- static/js/script.js
+++ static/js/script.js
@@ -112,7 +112,21 @@ const memberData = [
         profilePic: "/static/images/pfp-blastixxed.png",
         isOG: false,
         socials: [
-            { link: "https://osu.ppy.sh/users/35222110", icon: "bx bxs-music" }
+            { link: "https://osu.ppy.sh/users/35222110", icon: "bx bxs-music" },
+            { link: "https://etternaonline.com/users/Blastixxed", icon: "bx bxs-music" },
+            { link: "https://www.youtube.com/@sleepy.cereal", icon: "bx bxl-youtube" }
+        ]
+    },
+    
+    // örülsz
+    {
+        name: "mark",
+        role: "promoter",
+        profilePic: "/static/images/pfp-mark.png",
+        isOG: false,
+        socials: [
+            { link: "https://cshmark.xyz", icon: "bx bx-globe" },
+            { link: "https://www.youtube.com/@minmodeuser", icon: "bx bxl-youtube" }
         ]
     }
 ];
